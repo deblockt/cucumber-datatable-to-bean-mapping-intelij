010ad4225c82f85fa45fddd2f18dbb299545d388
Thomas Deblock <deblock.thomas.62@gmail.com>
2024-10-18 08:52:51 +0200

    feat: improve header row auto complete (#12)
    - automatically add pipe when it's needed
    - reformat datatable to match new header length
    - autocomplete only on datatable header row

--- src/main/kotlin/io/github/deblockt/cucumberdatatabletobeanmappingintelijplugin/DataTableCompletionContributor.kt
+++ src/main/kotlin/io/github/deblockt/cucumberdatatabletobeanmappingintelijplugin/DataTableCompletionContributor.kt
@@ -2,27 +2,10 @@ package io.github.deblockt.cucumberdatatabletobeanmappingintelijplugin
 
 import com.intellij.codeInsight.completion.*
 import com.intellij.codeInsight.lookup.LookupElementBuilder
-import com.intellij.openapi.util.TextRange
-import com.intellij.openapi.vfs.findFile
-import com.intellij.patterns.ElementPattern
-import com.intellij.patterns.ElementPatternCondition
-import com.intellij.patterns.InitialPatternCondition
 import com.intellij.patterns.PlatformPatterns
-import com.intellij.psi.*
-import com.intellij.psi.impl.source.PsiClassReferenceType
-import com.intellij.psi.search.EverythingGlobalScope
-import com.intellij.psi.search.FilenameIndex
-import com.intellij.psi.util.PsiTreeUtil
-import com.intellij.psi.util.contextOfType
-import com.intellij.psi.util.elementType
 import com.intellij.util.ProcessingContext
-import org.apache.commons.imaging.common.bytesource.ByteSourceInputStream
-import org.apache.lucene.util.fst.FST.BytesReader
-import org.jetbrains.plugins.cucumber.psi.GherkinElementType
-import org.jetbrains.plugins.cucumber.psi.GherkinElementTypes
 import org.jetbrains.plugins.cucumber.psi.GherkinTokenTypes
-import java.io.ByteArrayInputStream
-import java.util.Properties
+import org.jetbrains.plugins.cucumber.psi.impl.GherkinTableHeaderRowImpl
 
 
 class DataTableCompletionContributor: CompletionContributor() {
@@ -31,12 +14,14 @@ class DataTableCompletionContributor: CompletionContributor() {
         extend(
                 CompletionType.BASIC,
                 PlatformPatterns.psiElement()
-                        .withElementType(GherkinTokenTypes.TABLE_CELL),
+                        .withElementType(GherkinTokenTypes.TABLE_CELL)
+                        .withSuperParent(2, GherkinTableHeaderRowImpl::class.java),
                 object : CompletionProvider<CompletionParameters>() {
                     override fun addCompletions(parameters: CompletionParameters, context: ProcessingContext, resultSet: CompletionResultSet) {
                         datatableFields(parameters.position)
                                 .map {
-                                    val element = LookupElementBuilder.create(it.name)
+                                    val element = LookupElementBuilder.create(it.name.firstName())
+                                        .withInsertHandler(HeaderRowAutocompleteInsertHandler())
                                     if (it.description !== null) {
                                         element
                                                 .appendTailText(" ${it.description}", false)
